966e8efb15d7c7870eabf182aeb3fe1e32737abe
Jeremy PASCAL <jr28.my.p@gmail.com>
2019-01-15 10:30:54 +0100

    GET data

--- main.py
+++ main.py
@@ -1,3 +1,12 @@
 import api_key
+import urllib.request
+import json
 
-print(api_key.KEY)
+print("apiKey: " + api_key.KEY)
+
+json_contents = urllib.request.urlopen("https://api.jcdecaux.com/vls/v1/stations?apiKey=" + api_key.KEY +
+                                       "&contract=toulouse")
+
+contents = json.load(json_contents)
+
+print(contents)
